2564d11017b27dbbf84dcaa199f9e8f25784cc7c
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-05 21:17:25 -0400

    made testing for js

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+console.log("This is loading!");
+  // DOM VARIABLES
+
+  // JS VARIABLES
+
+  // FUNCTION DEFINITIONS
+
+  // FUNCTION CALLS
+
+  // EVENT LISTENERS
+});
