434a14f08b4026e84fbfd360f1289e3a3020aaf1
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-12 17:53:09 -0400

    added and tested ajax function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,10 +3,16 @@ console.log("This is loading!");
   // DOM VARIABLES
 
   // JS VARIABLES
-
+  var APIKey = "a06ddccb5d8eb173daab6e42a55bbeec";
   // FUNCTION DEFINITIONS
-
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Dunwoody,Georgia&appid=" + APIKey;
   // FUNCTION CALLS
-
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(queryURL);
+      console.log(response);
+    })
   // EVENT LISTENERS
 });
