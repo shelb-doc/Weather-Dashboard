4cf1bd10db65633cc23f00236eef3d0e66f7c9a3
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-13 18:01:17 -0400

    got icon to display

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,8 +4,12 @@ console.log("This is loading!");
 
   // JS VARIABLES
   var APIKey = "a06ddccb5d8eb173daab6e42a55bbeec";
-  // FUNCTION DEFINITIONS
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Dunwoody,Georgia&appid=" + APIKey;
+  
+  // FUNCTION DEFINITIONS
+  function kToF(temp){
+    return ((temp - 273.15) * 1.80 + 32)
+  }
   // FUNCTION CALLS
     $.ajax({
       url: queryURL,
@@ -14,17 +18,22 @@ console.log("This is loading!");
       console.log(queryURL);
       console.log(response);
       
-      $("#city").html("<h1>" + response.name + " Weather Details  </h1>");
+      $("#city").html("<h1>" + response.name + "</h1>");
+      
+      
       $("#wind").text("Wind Speed: " + response.wind.speed);
       $("#humidity").text("Humidity: " + response.main.humidity);
       
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-      var heatIndex = (response.main.feels_like - 273.15) * 1.80 + 32;
+      var iconurl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+      var tempF = kToF(response.main.temp);
+      var heatIndex = kToF(response.main.feels_like);
       
       $("#tempF").text("Temperature (F) " + tempF.toFixed(2));
       $("#heatIndex").text("Heat Index: " + heatIndex.toFixed(2));
+      $("#iconImg").attr("src", iconurl);
 
         console.log("Wind Speed: " + response.wind.speed);
+        console.log(iconurl);
         console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + tempF);
         console.log("Heat Index: " + response.main.feels_like);
