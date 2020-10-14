993c4f320e7ca5a5b6d59a0049ff31a37a2c19fc
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-13 23:02:42 -0400

    added cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,14 +4,17 @@ $(document).ready(function () {
 
   // JS VARIABLES
   var APIKey = "a06ddccb5d8eb173daab6e42a55bbeec";
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=Dunwoody,Georgia&appid=" +
-    APIKey;
+
   var currentdate = new Date();
   var datetime =  (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/" + currentdate.getFullYear();
   // FUNCTION DEFINITIONS
   function kToF(temp) {
     return (temp - 273.15) * 1.8 + 32;
+  }
+
+  function generateDate(modifier){
+    var currentdate = new Date();
+    return (currentdate.getMonth() + 1) + "/" + (currentdate.getDate() + modifier) + "/" + currentdate.getFullYear();
   }
 
   function getUVIndex(lon, lat) {
@@ -26,8 +29,8 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      console.log("UV Index: " + response.value);
       $("#UVIndex").text(response.value);
+      $("#UVIndex").removeClass();
       if(response.value<=2){
         $("#UVIndex").addClass("lowUV");
       } 
@@ -40,13 +43,15 @@ $(document).ready(function () {
     });
   }
   
-  function renderCurrentWeather(){
+  function renderCurrentWeather(city){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" +
+    APIKey;
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      console.log(queryURL);
-      console.log(response);
+      // console.log(queryURL);
+      // console.log(response);
       var iconUrl =
         "https://api.openweathermap.org/img/w/" +
         response.weather[0].icon +
@@ -61,17 +66,50 @@ $(document).ready(function () {
       $("#wind").text(response.wind.speed + " MPH");
       $("#heatIndex").text(heatIndex.toFixed(2) + " F");
       getUVIndex(response.coord.lon, response.coord.lat);
-      console.log("Wind Speed: " + response.wind.speed);
-      console.log(iconUrl);
-      console.log("Humidity: " + response.main.humidity);
-      console.log("longitude: " + response.coord.lon);
-      console.log("latitude: " + response.coord.lat);
-      console.log("Temperature (F): " + tempF);
-      console.log("Heat Index: " + response.main.feels_like);
+      // console.log("Wind Speed: " + response.wind.speed);
+      // console.log(iconUrl);
+      // console.log("Humidity: " + response.main.humidity);
+      // console.log("longitude: " + response.coord.lon);
+      // console.log("latitude: " + response.coord.lat);
+      // console.log("Temperature (F): " + tempF);
+      // console.log("Heat Index: " + response.main.feels_like);
+    });
+  }
+
+  function renderFiveDay(city){
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=" +
+    APIKey;
+    $( "#days-container" ).empty();
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      for (i=1; i<=5; i++){
+        currentInfo = response.list[i-1]
+        var iconUrl = "https://api.openweathermap.org/img/w/" +
+        currentInfo.weather[0].icon +
+        ".png";
+        var tempF = kToF(currentInfo.main.temp);
+        var humidity = currentInfo.main.humidity + "%"
+        var content = '<div class="card"><div class="card-body">'
+        content += '<h5 class="card-title">' + (generateDate(i)) + '</h5>';
+        content += '<img src="'+ iconUrl +'" />';
+        content += '<span>Temperature: ' + tempF.toFixed(2) + ' F</span><br>';
+        content += '<span>Humidity: ' + humidity + '</span>';
+        content += '</div></div>';
+        $('#days-container').append(content);
+      }
     });
   }
 
   // FUNCTION CALLS
-  renderCurrentWeather()
+  
   // EVENT LISTENERS
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    var city = $("#city-input").val();
+    console.log(city)
+    renderCurrentWeather(city)
+    renderFiveDay(city)
+  });
 });
